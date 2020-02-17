34319a4a34474e66ef8040a316959b8f0e832bc7
Alexey Nagorny <anagorny@wavesplatform.com>
2020-02-17 14:28:11 +0300

    :tada:

---
+++ index.ts
@@ -0,0 +1,16 @@
+const { order } = require('@waves/waves-transactions')
+
+const seed = 'b716885e9ba64442b4f1263c8e2d8671e98b800c60ec4dc2a27c83e5f9002b18'
+
+const params = {
+    amount: 100000000, //1 waves
+    price: 10, //for 0.00000010 BTC
+    priceAsset: '8LQW8f7P5d5PZM7GtZEBgaqRPGSzS3DfPuiXrURJ4AJS',
+    matcherPublicKey: '7kPFrHDiGw1rCm7LPszuECwWYL3dMf6iMifLRDJQZMzy',
+    orderType: 'buy'
+}
+
+
+const signedOrder = order(params, seed)
+
+console.log(signedOrder)
